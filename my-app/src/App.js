c4fe03af70c420b976af4ac0c5b86ccf37790b25
solaris5000 <solaris5000tv@gmail.com>
2024-11-25 19:58:25 +0500

    upd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -209,6 +209,8 @@ function App() {
       <div className="Footer">
         <div className="FooterContainer">
           <div className="logo"><a onClick={() => changeContent("homepage")}>SHOP.CO</a></div>
+          <br></br>
+          <br></br>
           <p>We have clothes that suits your style and which you’re proud to wear. From women to men.</p>
         </div>
         <div className="FooterContainer">
