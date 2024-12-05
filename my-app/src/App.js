0371638c64bf8cf6072aa2d5fdf9b943c15cf694
solaris5000 <solaris5000tv@gmail.com>
2024-12-05 21:46:34 +0500

    asd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -422,7 +422,7 @@ function App() {
             <div className="addToCart">
               <div className='btn-default counter'>
                 <button className='counterButton' onClick={handleButtonIncrease}>+</button>
-                {itemsCount}
+                <span id="countofitems">{itemsCount}</span>
                 <button className='counterButton' onClick={handleButtonDecrease}>-</button>
               </div>
               <button className="btn-active" style={{ width: '72%', height: '52px' }} onClick={() => {
